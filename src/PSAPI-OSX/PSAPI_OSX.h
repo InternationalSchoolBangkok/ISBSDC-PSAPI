c0e6cef8d503550ab0873290ac2b4acf79bebbd7
kolatat <kolatat.t@gmail.com>
2014-10-16 11:42:05 +0700

    Add target for OSX
    so applications compiled for OSX can import the library

---
+++ src/PSAPI-OSX/PSAPI_OSX.h
@@ -0,0 +1,13 @@
+//
+//  PSAPI_OSX.h
+//  PSAPI-OSX
+//
+//  Created by Kolatat Thangkasemvathana on 14/10/14.
+//  Copyright (c) 2014 ISB Software Development. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PSAPI_OSX : NSObject
+
+@end
---
+++ src/PSAPI-OSX/PSAPI_OSX.m
@@ -0,0 +1,13 @@
+//
+//  PSAPI_OSX.m
+//  PSAPI-OSX
+//
+//  Created by Kolatat Thangkasemvathana on 14/10/14.
+//  Copyright (c) 2014 ISB Software Development. All rights reserved.
+//
+
+#import "PSAPI_OSX.h"
+
+@implementation PSAPI_OSX
+
+@end
